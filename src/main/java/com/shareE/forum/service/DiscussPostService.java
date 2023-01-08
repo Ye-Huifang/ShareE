4337b92ad3cc8b86675ec2492a84cdfa02f537c1
Huifang-Ye <515108593@qq.com>
2023-01-07 23:38:07 -0500

    [Test] Unit test post related methods

--- src/main/java/com/shareE/forum/service/DiscussPostService.java
+++ src/main/java/com/shareE/forum/service/DiscussPostService.java
@@ -1,30 +1,101 @@
 package com.shareE.forum.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.shareE.forum.dao.DiscussMapper;
 import com.shareE.forum.entity.DiscussPost;
 import com.shareE.forum.util.SensitiveFilter;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 import org.unbescape.html.HtmlEscape;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
 
+	private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
 	@Autowired
 	private DiscussMapper discussMapper;
-
 
 	@Autowired
 	private SensitiveFilter sensitiveFilter;
 
+	@Value("${caffeine.post.max-size}")
+	private int maxSize;
+
+	@Value("${caffeine.post.expire-second}")
+	private int expireSeconds;
+
+	private LoadingCache<String, List<DiscussPost>> postListCache;
+
+	private LoadingCache<Integer, Integer> postRowsCache;
+
+	@PostConstruct
+	public void init() {
+		// initialize cache for post lists
+		postListCache = Caffeine.newBuilder()
+				.maximumSize(maxSize)
+				.expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, List<DiscussPost>>() {
+					@Override
+					public @Nullable List<DiscussPost> load(@NonNull String key) throws Exception {
+						if (key == null || key.length() == 0) {
+							throw new IllegalArgumentException("Argument illegal!");
+						}
+
+						String[] params = key.split(":");
+						if (params == null || params.length != 2) {
+							throw new IllegalArgumentException("Argument illegal!");
+						}
+
+						int offset = Integer.valueOf(params[0]);
+						int limit = Integer.valueOf(params[1]);
+
+						logger.debug("load list rows from DB");
+						return discussMapper.selectDiscussPosts(0, offset, limit, 1);
+					}
+				});
+
+		// initialize cache for post row
+		postRowsCache = Caffeine.newBuilder()
+				.maximumSize(maxSize)
+				.expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+				.build(new CacheLoader<Integer, Integer>() {
+					       @Override
+					       public @Nullable Integer load(@NonNull Integer key) throws Exception {
+						       logger.debug("load post rows from DB");
+						       return discussMapper.selectDiscussPostRows(key);
+					       }
+				       }
+				);
+	}
+
 	public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
+		if (userId == 0 && orderMode == 1) {
+			return postListCache.get(offset + ":" + limit);
+		}
+
+		logger.debug("load post list from DB");
 		return discussMapper.selectDiscussPosts(userId, offset, limit, orderMode);
 	}
 
 	public int findDiscussPostRows(int userId) {
+		if (userId == 0) {
+			return postRowsCache.get(userId);
+		}
+
+		logger.debug("load post rows from DB");
 		return discussMapper.selectDiscussPostRows(userId);
 	}
 
--- src/test/java/com/shareE/forum/CaffeineTests.java
+++ src/test/java/com/shareE/forum/CaffeineTests.java
@@ -1,2 +1,44 @@
-package com.shareE.forum;public class CaffeineTests {
+package com.shareE.forum;
+
+import com.shareE.forum.entity.DiscussPost;
+import com.shareE.forum.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class CaffeineTests {
+
+	@Autowired
+	private DiscussPostService discussPostService;
+
+	@Test
+	public void initDataForTest() {
+		for (int i = 0; i < 300000; i++) {
+			DiscussPost post = new DiscussPost();
+			post.setUserId(111);
+			post.setTitle("Test Caffeine");
+			post.setContent("Test Caffeine Content");
+			post.setCreateTime(new Date());
+			post.setScore(Math.random() * 2000);
+			discussPostService.addDiscussPost(post);
+		}
+	}
+
+	@Test
+	public void testCache() {
+		System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+		System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+		System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+		System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 0));
+
+	}
+
 }
