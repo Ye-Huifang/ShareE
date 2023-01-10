d5e52dfeaab7250b897eb1796fd54d75a6033332
Huifang-Ye <515108593@qq.com>
2023-01-09 21:46:02 -0500

    [Performance] Wrap up the project

--- src/main/java/com/shareE/forum/config/SecurityConfig.java
+++ src/main/java/com/shareE/forum/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Foru
 				)
 				.antMatchers(
 						"/discuss/delete",
-						"/data/**"
+						"/data/**",
+						"/actuator/**"
 				)
 				.hasAnyAuthority(
 						AUTHORITY_ADMIN
---
+++ target/classes/static/js/index 2.js
@@ -0,0 +1,32 @@
+$(function(){
+	$("#publishBtn").click(publish);
+});
+
+function publish() {
+	$("#publishModal").modal("hide");
+
+	// get title and content
+	var title = $("#recipient-name").val();
+	var content = $("#message-text").val();
+	// send async request (POST)
+	$.post(
+		"/forum/discuss/add",
+		{"title": title, "content": content},
+		function (data) {
+			data = $.parseJSON(data);
+			$("#hintBody").text(data.msg);
+			// show the hint modal
+			$("#hintModal").modal("show");
+			// after 2 seconds, hind the hint modal
+			setTimeout(function(){
+				$("#hintModal").modal("hide");
+				// refresh the current page
+				if (data.code == 0) {
+					window.location.reload();
+				}
+			}, 2000);
+		}
+	);
+
+
+}
